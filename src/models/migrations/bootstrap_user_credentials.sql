a850b4bacc66f5c3bff251d1fa650729b327b16e
isch <lesterfrance@gmail.com>
2024-02-26 16:24:41 +0300

    fixed bootstrap data

--- src/models/migrations/bootstrap_user_credentials.sql
+++ src/models/migrations/bootstrap_user_credentials.sql
@@ -1,1 +1,1 @@
-INSERT INTO ph_qa_manager.user_credentials (id, password, user_id) VALUES ('4', '$2a$10$oWVzHTdjpdz7aQ/cIAdz2.wRMwoCIDVDLijFIw2P9uBjUVE1MfNkO', 4);
+INSERT INTO ph_qa_manager.user_credentials (id, password, user_id) VALUES ('1', '$2a$10$oWVzHTdjpdz7aQ/cIAdz2.wRMwoCIDVDLijFIw2P9uBjUVE1MfNkO', 1);
